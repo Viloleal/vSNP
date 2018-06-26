0978f7119444d1ed17f4de120b5d42a047574640
stuber <tod.p.stuber@usda.gov>
2018-06-26 07:05:32 -0500

    check read size for spoligo repeat capture

--- vSNP.py
+++ vSNP.py
@@ -934,10 +934,13 @@ class script1():
                     # all 3, title and seq and qual, were needed
                     for title, seq, qual in FastqGeneralIterator(in_handle):
                         sequence_list.append(seq)
-
-            capture_spacer_sequence = re.compile(".*TTTCCGTCCC.*|.*GGGACGGAAA.*|.*TCTCGGGGTT.*|.*AACCCCGAGA.*|.*TGGGTCTGAC.*|.*GTCAGACCCA.*")
-            sequence_list = list(filter(capture_spacer_sequence.match, sequence_list))
-            seq_string = "".join(sequence_list)
+            if len(seq) > 99:
+                capture_spacer_sequence = re.compile(".*TTTCCGTCCC.*|.*GGGACGGAAA.*|.*TCTCGGGGTT.*|.*AACCCCGAGA.*|.*TGGGTCTGAC.*|.*GTCAGACCCA.*")
+                sequence_list = list(filter(capture_spacer_sequence.match, sequence_list))
+                seq_string = "".join(sequence_list)
+            else:
+                #if < 100 then search all reads, not just those with repeat regions.
+                seq_string = "".join(sequence_list)
 
             # for i in fastqs: #remove unzipped fastq files to save space
             os.remove(R1unzip)

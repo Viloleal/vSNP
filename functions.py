a1cf87d87b5712c70be54d7f1efa9a80dedec2a8
stuber <tod.p.stuber@usda.gov>
2018-11-08 12:27:13 -0600

    add -e 1 parameter to freebayes decreasing influence of reads with N gaps

--- functions.py
+++ functions.py
@@ -608,7 +608,7 @@ def align_reads(arg_options):
             #     last_number = pos
             # print("{}:{}-{}" .format(chrom, pos, total_len), file=chrom_ranges)
         chrom_ranges.close()
-        os.system(r'freebayes-parallel chrom_ranges.txt 8 -E -1 --strict-vcf -u -f %s %s > %s' % (sample_reference, nodupbam, unfiltered_hapall))
+        os.system(r'freebayes-parallel chrom_ranges.txt 8 -E -1 -e 1 -u --strict-vcf -f %s %s > %s' % (sample_reference, nodupbam, unfiltered_hapall))
         # "fix" MQ notation in VCF to match GATK output
         write_fix = open(mapq_fix, 'w+')
         with open(unfiltered_hapall, 'r') as unfiltered:
